8c59facc85e4ac2e2369dfc61a5036aaf3f789e1
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-03-31 07:15:59 +0700

    Fixed scrolling issues at homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@theme/Layout"
 
 export default function Page(){
     return <Layout>
-        <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <div className="w-full h-full flex flex-col justify-center items-center">
             <div className="flex flex-col items-center">
                 <h1 className="text-5xl">Welcome to ⁋ Base!</h1>
                 <a href="/archive">
